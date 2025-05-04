3d8215fa2eec6c9a5cdc5a507dd3ffdad32bd320
Russi <russi.konsultan@work.bri.co.id>
2025-05-04 21:28:04 +0700

    Update Version DB

--- app/src/main/java/id/herdroid/ecommerce/data/local/AppDatabase.kt
+++ app/src/main/java/id/herdroid/ecommerce/data/local/AppDatabase.kt
@@ -7,20 +7,21 @@ import id.herdroid.ecommerce.data.local.dao.FavoriteDao
 import id.herdroid.ecommerce.data.local.dao.OrderDao
 import id.herdroid.ecommerce.data.local.entity.CartProductEntity
 import id.herdroid.ecommerce.data.local.entity.FavoriteProduct
+import id.herdroid.ecommerce.data.local.entity.OrderDetailsEntity
 import id.herdroid.ecommerce.data.local.entity.OrderEntity
 
 @Database(
     entities = [
         FavoriteProduct::class,
         CartProductEntity::class,
-        OrderEntity::class
+        OrderEntity::class,
+        OrderDetailsEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
     abstract fun cartDao(): CartDao
     abstract fun orderDao(): OrderDao
-
 }
